f827d76960d4ef232f57b0fd4e066da4f8db4485
DBPerera22 <denukaperera49@gmail.com>
2023-06-24 10:37:30 +0530

    Final Commit

--- Asignment-backend/Controllers/MontyHallController.cs
+++ Asignment-backend/Controllers/MontyHallController.cs
@@ -1,4 +1,6 @@
 ﻿using Asignment_backend;
+using Asignment_backend.Controllers;
+using Assignment_backend;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
@@ -10,36 +12,88 @@ namespace Assignment_backend.Controllers
     public class MontyHallController : ControllerBase
     {
         private static readonly Random random = new Random();
+        private readonly ILogger<MontyHallController> _logger;
 
-        [HttpPost("simulate")]
-        public IActionResult SimulateGame(MontyHallGame game)
+        public MontyHallController(ILogger<MontyHallController> logger)
         {
-        
-            // Perform the game simulation
-            int prizeDoor = random.Next(1, game.NumberOfDoors + 1);
-            int[] doors = Enumerable.Range(1, game.NumberOfDoors).ToArray();
+            _logger = logger;
+        }
+        [HttpPost("play")]
+        public IActionResult PlayGame([FromBody] MontyHallGame game)
+        {
+            int prizeDoor = random.Next(1, 4); // Fixed number of doors (3)
+            int[] doors = new int[] { 1, 2, 3 }; // Fixed number of doors (3)
+
             int[] availableDoors = doors.Where(d => d != game.InitialDoorSelection && d != prizeDoor).ToArray();
             int revealedDoor = availableDoors[random.Next(0, availableDoors.Length)];
 
-            // Update the game state
-            game.RevealedDoor = revealedDoor;
-            game.FinalDoor = game.InitialDoorSelection; // Assuming the player sticks with the initial choice
-            game.IsWin = game.FinalDoor == prizeDoor; // Check if the final door matches the prize door
+            int finalDoor;
+            if (game.ChangeDoor)
+                finalDoor = doors.Single(d => d != game.InitialDoorSelection && d != revealedDoor);
+            else
+                finalDoor = game.InitialDoorSelection;
 
-            return Ok(game);
+            bool isWin = finalDoor == prizeDoor;
+
+            var response = new
+            {
+                InitialDoorSelection = game.InitialDoorSelection,
+                RevealedDoor = revealedDoor,
+                FinalDoor = finalDoor,
+                IsWin = isWin,
+                CorrectDoor = prizeDoor
+            };
+
+            return Ok(response);
         }
 
 
-        [HttpGet("reveal")]
-        public MontyHallGame RevealDoor([FromQuery] MontyHallGame game)
+        [HttpGet("hello")]
+        public string HelloWorld()
         {
-  
-            // Update the game state with the final door selection
-            int[] doors = Enumerable.Range(1, game.NumberOfDoors).ToArray();
-            game.FinalDoor = doors.Single(d => d != game.InitialDoorSelection && d != game.RevealedDoor);
-            game.IsWin = game.FinalDoor == random.Next(1, game.NumberOfDoors + 1);
+           
 
-            return game;
+            return "Hello World";
         }
+
+        //[HttpPost("simulate")]
+        //public IActionResult SimulateGames([FromBody] MontyHallGame game)
+        //{
+        //    int numSimulations = game.NumSimulations;
+        //    bool changeDoor = game.ChangeDoor;
+
+        //    int wins = 0;
+        //    int losses = 0;
+
+        //    for (int i = 0; i < numSimulations; i++)
+        //    {
+        //        int prizeDoor = random.Next(1, 4); // Fixed number of doors (3)
+        //        int[] doors = new int[] { 1, 2, 3 }; // Fixed number of doors (3)
+
+        //        int[] availableDoors = doors.Where(d => d != game.InitialDoorSelection && d != prizeDoor).ToArray();
+        //        int revealedDoor = availableDoors[random.Next(0, availableDoors.Length)];
+
+        //        int finalDoor;
+        //        if (changeDoor)
+        //            finalDoor = doors.Single(d => d != game.InitialDoorSelection && d != revealedDoor);
+        //        else
+        //            finalDoor = game.InitialDoorSelection;
+
+        //        if (finalDoor == prizeDoor)
+        //            wins++;
+        //        else
+        //            losses++;
+        //    }
+
+        //    var response = new
+        //    {
+        //        NumSimulations = numSimulations,
+        //        Wins = wins,
+        //        Losses = losses
+        //    };
+
+        //    return Ok(response);
+        //}
+
     }
 }
--- Asignment-backend/MontyHallGame.cs
+++ Asignment-backend/MontyHallGame.cs
@@ -2,11 +2,9 @@
 {
     public class MontyHallGame
     {
-        public int NumberOfDoors { get; set; }
         public int InitialDoorSelection { get; set; }
-        public int RevealedDoor { get; set; }
-        public int FinalDoor { get; set; }
-        public bool IsWin { get; set; }
+        public bool ChangeDoor { get; set; }
     }
+
 
 }
